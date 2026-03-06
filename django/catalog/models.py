c9486b41c9a7f644351ff4600cbad9aecc92f077
dosimx <aidaralina7@gmail.com>
2026-03-06 23:27:50 +0500

    добавил базовое джанго

---
+++ django/catalog/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    slug = models.SlugField(max_length=120, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Cake(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='cakes')
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    weight = models.PositiveIntegerField(help_text='В граммах')
+    image = models.ImageField(upload_to='cakes/')
+    is_available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
